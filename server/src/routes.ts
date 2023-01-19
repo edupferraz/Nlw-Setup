c801128819f9d54572916878f8b7ca481d9860f7
Eduardo Ferraz <edupferraz@gmail.com>
2023-01-19 08:39:35 -0300

    Back-end - Day 2

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import { FastifyInstance } from "fastify"
 import {z} from 'zod'
 import { prisma } from "./lib/prisma"
@@ -8,9 +9,7 @@ export async function appRoutes(app: FastifyInstance) {
         const createHabitBody = z.object({
             title: z.string(),
             weekDays: z.array(
-                z.number()
-                    .min(0)
-                    .max(6)
+                z.number().min(0).max(6)
             )
         })
 
@@ -21,7 +20,7 @@ export async function appRoutes(app: FastifyInstance) {
         await prisma.habit.create({
             data: {
                 title, 
-                created_at: new Date(),
+                created_at: today,
                 weekDays: {
                     create: weekDays.map(weekDay => {
                         return {
@@ -34,5 +33,48 @@ export async function appRoutes(app: FastifyInstance) {
 
     })
     
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits
+        }
+    })
 }
 
