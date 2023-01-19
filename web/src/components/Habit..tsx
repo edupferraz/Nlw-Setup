6b685d7b077a67481f023913bfb759abc5b6828e
Eduardo Ferraz <edupferraz@gmail.com>
2023-01-19 09:18:57 -0300

    Web - Day 2

--- web/src/components/Habit..tsx
+++
@@ -1,9 +0,0 @@
-interface HabitProps{
-    completed: number
-}
-
-export function Habit(props: HabitProps) {
-    return(
-        <h1 className="bg-slate-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">{props.completed}</h1>
-    )
-}
